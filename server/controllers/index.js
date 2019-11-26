0431639ff965df26e378885d3c75d96eeb3ffa6c
rana <ranasobeid95@gmail.com>
2019-11-26 18:28:11 +0200

    add sign-up route and require signup middleware #12

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const { getEmptyBuildings, postEmptyBuilding } = require('./emptyBuildings');
 const mailList = require('./mailList');
+const { signUp } = require('./users');
 
 router.get('/empty-buildings', getEmptyBuildings);
 router.get('/mailList', mailList);
 
+router.post('/sign-up', signUp);
 router.post('/report-building', postEmptyBuilding);
 
 module.exports = router;
