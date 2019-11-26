934ff549595cf7629ffbd67e5415dcdde14dd632
rana <ranasobeid95@gmail.com>
2019-11-26 18:24:57 +0200

    add record to user table in airtable #12

--- server/models/queries/index.js
+++ server/models/queries/index.js
@@ -1,7 +1,9 @@
 const { get: getEmptyBuildings } = require('./emptyBuildings');
+const { postUsers } = require('./users');
 const mailList = require('./mailList');
 
 module.exports = {
   getEmptyBuildings,
+  postUsers,
   mailList,
 };
