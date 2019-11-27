f0b717101bd8064e7fa5450cfc65a0979a098f74
rana <ranasobeid95@gmail.com>
2019-11-27 15:31:52 +0200

    reject sign in if email already registered #12

--- server/controllers/users/singUp.js
+++ server/controllers/users/singUp.js
@@ -1,33 +1,38 @@
-const jwt = require('jsonwebtoken');
+const { sign } = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
-const { postUsers } = require('../../models/queries/users');
+const { postUsers, getUserByEmail } = require('../../models/queries/users');
 const { signUpSchema } = require('../../validation/userSchema');
 
 module.exports = async (req, res, next) => {
-  const SecretKey = process.env.SECRET_KEY;
+  const key = process.env.KEY;
 
   try {
-    const userInfo = await signUpSchema.validate(req.body, {
-      abortEarly: false,
-    });
-    const { email, password: userPassword, username } = userInfo;
-    const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(userPassword, salt);
-    const addUser = await postUsers({ email, hashPassword, username });
-
-    const [
-      {
-        fields: { password },
-      },
-    ] = addUser;
-    const token = jwt.sign({ password }, SecretKey);
-    res.cookie('access', token);
-    res.status(201).send({
-      statusCode: 201,
-      message: 'user was sign up successfully',
-      data: { username, email },
-    });
+    const { email: newEmail } = req.body;
+    const userExist = await getUserByEmail(newEmail);
+    if (userExist) {
+      res.send({
+        statusCode: 200,
+        message: 'Email already exists',
+        data: { newEmail },
+      });
+    } else {
+      const userInfo = await signUpSchema.validate(req.body, {
+        abortEarly: false,
+      });
+      const { email, password: userPassword, username } = userInfo;
+      const salt = await bcrypt.genSalt(10);
+      const hashPassword = await bcrypt.hash(userPassword, salt);
+      const addUser = await postUsers({ email, hashPassword, username });
+      const [{ id }] = addUser;
+      const token = sign({ userId: id }, key);
+      res.cookie('token', token, { maxAge: 8400000, httpOnly: true });
+      res.status(201).send({
+        statusCode: 201,
+        message: 'user was sign up successfully',
+        data: { username, email },
+      });
+    }
   } catch (error) {
     if (error.name === 'ValidationError')
       res.status(400).send({ statusCode: 400, error: error.errors });
