39e779cfdd5a118b6a5f5ad5c65b6af6d9290d38
rana <ranasobeid95@gmail.com>
2019-11-28 09:57:34 +0200

    update style of sign-up page #18

--- client/src/components/pages/SignUp/index.js
+++ client/src/components/pages/SignUp/index.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Form, Input, Tooltip, Icon, Button } from 'antd';
+import axios from 'axios';
 
+import { Navbar } from 'components/utils';
 import styles from './signUp.module.css';
 
 class SignUp extends Component {
@@ -15,9 +17,9 @@ class SignUp extends Component {
     const {
       form: { validateFieldsAndScroll },
     } = this.props;
-    validateFieldsAndScroll((err, values) => {
+    validateFieldsAndScroll(async (err, values) => {
       if (!err) {
-        console.log('Received values of form: ', values);
+        // const vlaues = axios.post('/api/v1/sign-up');
       }
     });
   };
@@ -52,83 +54,89 @@ class SignUp extends Component {
       form: { getFieldDecorator },
     } = this.props;
     return (
-      <div className={`${styles.signUp} signUp-controller`}>
-        <h1 className={styles.heading}>Create Your Account</h1>
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Item label="E-mail">
-            {getFieldDecorator('email', {
-              rules: [
-                {
-                  type: 'email',
-                  message: 'The input is not valid E-mail!',
-                },
-                {
-                  required: true,
-                  message: 'Please input your E-mail!',
-                },
-              ],
-            })(<Input />)}
-          </Form.Item>
-          <Form.Item label="Password" hasFeedback>
-            {getFieldDecorator('password', {
-              rules: [
-                {
-                  required: true,
-                  message: 'Please input your password!',
-                },
-                {
-                  validator: this.validateToNextPassword,
-                },
-              ],
-            })(<Input.Password />)}
-          </Form.Item>
-          <Form.Item label="Confirm Password" hasFeedback>
-            {getFieldDecorator('confirm', {
-              rules: [
-                {
-                  required: true,
-                  message: 'Please confirm your password!',
-                },
-                {
-                  validator: this.compareToFirstPassword,
-                },
-              ],
-            })(<Input.Password onBlur={this.handleConfirmBlur} />)}
-          </Form.Item>
-          <Form.Item
-            label={
-              <span>
-                Username&nbsp;
-                <Tooltip title="What do you want others to call you?">
-                  <Icon type="question-circle-o" />
-                </Tooltip>
-              </span>
-            }
-          >
-            {getFieldDecorator('nickname', {
-              rules: [
-                {
-                  required: true,
-                  message: 'Please input your nickname!',
-                  whitespace: true,
-                },
-              ],
-            })(<Input />)}
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" block>
-              Sign Up
-            </Button>
-            <div className={styles.hasAccount}>
-              <span>
-                Already a member ?
-                <Link to="/sign-in">
-                  <h4 className={styles.signin}>Sign in</h4>
-                </Link>
-              </span>
-            </div>
-          </Form.Item>
-        </Form>
+      <div className={`${styles.signUpPage} signUp-controller`}>
+        <Navbar />
+        <div className={`${styles.signUp} signUp-controller`}>
+          <div className={styles.logoSection} />
+          <div className={styles.formSection}>
+            <h1 className={styles.heading}>Create Your Account</h1>
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Item label="E-mail">
+                {getFieldDecorator('email', {
+                  rules: [
+                    {
+                      type: 'email',
+                      message: 'The input is not valid E-mail!',
+                    },
+                    {
+                      required: true,
+                      message: 'Please input your E-mail!',
+                    },
+                  ],
+                })(<Input />)}
+              </Form.Item>
+              <Form.Item label="Password" hasFeedback>
+                {getFieldDecorator('password', {
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Please input your password!',
+                    },
+                    {
+                      validator: this.validateToNextPassword,
+                    },
+                  ],
+                })(<Input.Password />)}
+              </Form.Item>
+              <Form.Item label="Confirm Password" hasFeedback>
+                {getFieldDecorator('confirm', {
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Please confirm your password!',
+                    },
+                    {
+                      validator: this.compareToFirstPassword,
+                    },
+                  ],
+                })(<Input.Password onBlur={this.handleConfirmBlur} />)}
+              </Form.Item>
+              <Form.Item
+                label={
+                  <span>
+                    Username&nbsp;
+                    <Tooltip title="What do you want others to call you?">
+                      <Icon type="question-circle-o" />
+                    </Tooltip>
+                  </span>
+                }
+              >
+                {getFieldDecorator('nickname', {
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Please input your nickname!',
+                      whitespace: true,
+                    },
+                  ],
+                })(<Input />)}
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit" block>
+                  Sign Up
+                </Button>
+                <div className={styles.hasAccount}>
+                  <span>
+                    Already a member?
+                    <Link to="/sign-in">
+                      <h4 className={styles.signin}>Sign in</h4>
+                    </Link>
+                  </span>
+                </div>
+              </Form.Item>
+            </Form>
+          </div>
+        </div>
       </div>
     );
   }
