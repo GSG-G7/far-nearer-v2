29492be9867b0482ff4b4e04dc037f3e17650945
rana <ranasobeid95@gmail.com>
2019-11-28 10:00:01 +0200

    fix eslint error #18

--- client/src/components/pages/SignUp/index.js
+++ client/src/components/pages/SignUp/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Form, Input, Tooltip, Icon, Button } from 'antd';
-import axios from 'axios';
 
 import { Navbar } from 'components/utils';
 import styles from './signUp.module.css';
